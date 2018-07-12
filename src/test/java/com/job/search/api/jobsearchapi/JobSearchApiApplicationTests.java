057c90d0e5a3c3f0085c0e56001a1e431962cb24
Tolu Idowu <itolu94@gmail.com>
2018-07-12 19:32:42 -0400

    tidy up

--- src/test/java/com/job/search/api/jobsearchapi/JobSearchApiApplicationTests.java
+++ src/test/java/com/job/search/api/jobsearchapi/JobSearchApiApplicationTests.java
@@ -1,12 +1,18 @@
 package com.job.search.api.jobsearchapi;
 
+import com.job.search.api.jobsearchapi.API.CyberCodersAPI;
+import com.job.search.api.jobsearchapi.API.DiceAPI;
+import com.job.search.api.jobsearchapi.API.ZipRecruiterAPI;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+
 @SpringBootTest
+@WebMvcTest({DiceAPI.class, ZipRecruiterAPI.class, CyberCodersAPI.class})
 public class JobSearchApiApplicationTests {
 
 	@Test
